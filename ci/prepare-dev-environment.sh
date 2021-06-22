463e1e8f617ed06e50c74b3afa1e9102bd60e826
Greg Banks <gnb@fmeh.org>
2021-06-22 19:21:02 -0400

    The Ubuntu container doesn't have gcc installed

--- ci/prepare-dev-environment.sh
+++ ci/prepare-dev-environment.sh
@@ -6,9 +6,11 @@ host_os=$(uname -s)
 case "$host_os" in
 Linux)
     if [ -f /etc/ubuntu-release ] ; then
-        sudo apt-get install autoconf automake make
+        sudo apt-get install \
+            autoconf automake make gcc
     elif [ -f /etc/redhat-release ] ; then
-        sudo yum install -y autoconf automake make
+        sudo yum install -y \
+            autoconf automake make
     fi
     ;;
 Darwin)
