9e200bfefeafd2cd398249fbe0c4efb38e350d5f
Greg Banks <gnb@fmeh.org>
2021-06-22 19:36:45 -0400

    Detect Ubuntu properly

--- ci/prepare-dev-environment.sh
+++ ci/prepare-dev-environment.sh
@@ -3,12 +3,22 @@
 set -x
 host_os=$(uname -s)
 
+function is_ubuntu()
+{
+    grep Ubuntu /etc/issue > /dev/null 2>&1
+}
+
+function is_redhat()
+{
+    [ -f /etc/redhat-release ]
+}
+
 case "$host_os" in
 Linux)
-    if [ -f /etc/ubuntu-release ] ; then
+    if is_ubuntu ; then
         sudo apt-get install \
             autoconf automake make gcc
-    elif [ -f /etc/redhat-release ] ; then
+    elif is_redhat ; then
         sudo yum install -y \
             autoconf automake make
     fi
